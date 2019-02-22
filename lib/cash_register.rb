3697f67a40bc23efcb4c14d3963b409b072773df
Vinh Do <vinhdo07@gmail.com>
2019-02-22 21:45:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = 1)
-    @item, @price, @quantity = item, price, quantity
+    @price, @quantity = price, quantity
     @total += @price * @quantity
     @quantity.times {@cart << item}
   end
